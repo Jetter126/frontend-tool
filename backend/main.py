56fb79b72b74b4a8eb52bfac551933d48f494df4
Jetter126 <83302410+Jetter126@users.noreply.github.com>
2025-06-07 16:11:17 +0530

    added functionality to clean sample website URL

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,14 @@
-from utils import find_tech_stack
-import json
+from langgraph.graph import StateGraph
+from typing import Optional, TypedDict, List
 
-url = "https://www.github.com/"
-result = find_tech_stack(url)
-print(result)
+from utils import clean_url
+
+# State definition
+class State(TypedDict):
+    sample_website: str
+    tech_stack: List[str]
+
+# Node functions
+def clean_sample_website(state: State):
+    state["sample_website"] = clean_url(state["sample_website"])
+    return state 
--- backend/utils.py
+++ backend/utils.py
@@ -2,7 +2,31 @@ import requests, os
 
 builtwith_api_key = os.environ["BUILTWITH_API_KEY"]
 
-def find_tech_stack(url):
+def clean_url(url: str) -> str | int:
+    """Returns a clean version of the input URL (formatted as https://example.com/) or 
+    -1 if the input URL doesn't contain a protocol.
+    """
+    if url.startswith("http://"):
+        protocol = "http://"
+        url = url[len(protocol):]
+    elif url.startswith("https://"):
+        protocol = "https://"
+        url = url[len(protocol):]
+    else:
+        return -1
+    
+    # Remove anything after the domain and www.
+    for sep in ["/", "?", "#"]:
+        if sep in url:
+            url = url.split(sep)[0]
+    if url.startswith("www."):
+        url = url[len("www."):]
+
+    # Return the clean URL
+    return f"{protocol}{url}/"
+    
+
+def find_tech_stack(url: str):
     """Uses a website's URL to find the tech stack used to build it"""
 
     api_url = f"https://api.builtwith.com/free1/api.json?KEY={builtwith_api_key}&LOOKUP={url}"
