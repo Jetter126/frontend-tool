0a984aa03b4c1930ec2d621771bd41a7250b8fab
Jetter126 <83302410+Jetter126@users.noreply.github.com>
2025-06-09 21:25:06 +0530

    added the graph builder

--- backend/main.py
+++ backend/main.py
@@ -12,19 +12,28 @@ class State(TypedDict):
     error_message: str
 
 # Node functions
-def error_function(state: State):
+def error_function(state: State) -> State:
     print(state["error_message"])
     return state
 
-def clean_sample_website(state: State):
-    if validators.url(state["sample_website"]):
-        result = clean_url(state["sample_website"])
-    else:
-        state["error_message"] = "The given URL is invalid."
-    
-    if int(requests.get(result).status_code) / 100 == 2:
+def clean_sample_website(state: State) -> State:
+    result = clean_url(state["sample_website"])
+    if validators.url(result) and int(requests.get(result).status_code) / 100 == 2:
         state["sample_website"] = result
     else:
         state["error_message"] = "The given URL is invalid."
 
     return state
+
+# Building the graph
+graph_builder = StateGraph(State)
+
+graph_builder.add_node("clean_sample_website", clean_sample_website)
+
+graph_builder.add_edge(START, "clean_sample_website")
+graph_builder.add_edge("clean_sample_website", END)
+
+graph = graph_builder.compile()
+
+result = graph.invoke({"sample_website": "https://www.github.com/homepage?view=123/123"})
+print(result["sample_website"])
