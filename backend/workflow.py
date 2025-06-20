913e10ccbbf099abc410a489d3a1c125703e1a53
Jetter126 <83302410+Jetter126@users.noreply.github.com>
2025-06-20 17:42:07 +0530

    added _extract_tech_stack() and updated the workflow

--- backend/workflow.py
+++ backend/workflow.py
@@ -7,7 +7,7 @@ from langgraph.graph import StateGraph, START, END
 import validators
 
 from models import State
-from utils import clean_url
+from utils import clean_url, extract_tech_stack
 
 
 class Workflow:
@@ -16,26 +16,33 @@ class Workflow:
         self.workflow = self._build_workflow()
 
     def _build_workflow(self):
-        """Builds the workflow graph using nodes and edges"""
+        """Builds the workflow graph using nodes and edges."""
         graph_builder = StateGraph(State)
 
         graph_builder.add_node("clean_sample_website", self._clean_sample_website)
+        graph_builder.add_node("extract_tech_stack", self._extract_tech_stack)
 
         graph_builder.add_edge(START, "clean_sample_website")
-        graph_builder.add_edge("clean_sample_website", END)
+        graph_builder.add_edge("clean_sample_website", "extract_tech_stack")
+        graph_builder.add_edge("extract_tech_stack", END)
 
         return graph_builder.compile()
 
     def _clean_sample_website(self, state: State) -> Dict[str, Any]:
-        """Cleans and validates the sample website submitted by the user"""
+        """Cleans and validates the sample website submitted by the user."""
         result = clean_url(state.sample_website)
         if validators.url(result) and int(requests.get(result).status_code / 100) == 2:
             return {"sample_website": result}
         else:
             return {"error_message": "The given URL is invalid."}
+        
+    def _extract_tech_stack(self, state: State) -> Dict[str, Any]:
+        """Extracts the tech stack used to build the sample website."""
+        tech_stack = extract_tech_stack(state.sample_website)
+        return {"tech_stack": tech_stack}
 
     def run(self, sample_website: str):
-        """Takes in user input and runs the agent"""
+        """Takes in user input and runs the agent."""
         initial_state = State(sample_website=sample_website)
         final_state = self.workflow.invoke(initial_state)
         return State(**final_state)
