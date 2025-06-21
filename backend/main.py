1b33602b2ad3ad4a73c8d749ef0385dd9cc7311f
Jetter126 <83302410+Jetter126@users.noreply.github.com>
2025-06-22 00:15:14 +0530

    deleted print statements

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,5 @@
 from workflow import Workflow
 
 workflow = Workflow()
-result = workflow.run("https://www.github.com/homepage?view=123/123")
-print(result.sample_website)
-print(result.tech_stack)
+website = "https://www.github.com/homepage?view=123/123"
+result = workflow.run(website)
